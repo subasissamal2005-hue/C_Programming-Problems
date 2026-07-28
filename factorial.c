d9fd9e47c05be9c2c76d063c642f1b0287b10c99
subasissamal2005-hue <subasis.samal2005@gmail.com>
2026-07-28 19:38:07 +0530

    factorial of number

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+
+    printf("enter a number:");
+    scanf("%d",&n);
+
+    int fact = 1;
+
+    for(int i=1;i<=n;i++){
+        fact = fact * i;
+        
+    }
+
+    printf("factorial of no is %d",fact);
+
+    return 0;
+}
