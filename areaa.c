5b14b6717799b3810d72dc9a73dafde68ecac231
subasissamal2005-hue <subasis.samal2005@gmail.com>
2026-07-28 19:42:35 +0530

    are of a radius

---
+++ areaa.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float radius;
+    
+    printf("enter radius");
+    scanf("%f",&radius);
+
+    printf("radius is: %f",3.14 * radius * radius);
+    return 0;
+}
